98e79f8a54528533b7b3eecbba3a5f09b056903b
Ana Beatriz Arteiro Barreiro <anabeatrizarteiro@gmail.com>
2025-11-19 14:42:08 -0300

    Ajuste e finalização da tela de login

--- production/src/app/componentes/telaInicial/telaInicial.ts
+++ production/src/app/componentes/telaInicial/telaInicial.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-telaInicial',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './telaInicial.html',
   styleUrl: './telaInicial.scss'
 })
