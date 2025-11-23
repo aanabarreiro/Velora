f7c3816dd7f1b284494cb9b0d51b9b9f00b7af49
Ana Beatriz Arteiro Barreiro <anabeatrizarteiro@gmail.com>
2025-11-23 15:02:26 -0300

    Continuação do projeto

--- production/src/app/app.routes.ts
+++ production/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes } from '@angular/router';
 import {Login} from './componentes/login/login';
 import { Cadastro } from './componentes/cadastro/cadastro';
 import { TelaInicial } from './componentes/telaInicial/telaInicial';
+import { TelaInicialEncarregados } from './componentes/tela-inicial-encarregados/tela-inicial-encarregados';
 
 export const routes: Routes = [
     {path: '', component: Login},
     {path: 'cadastro', component: Cadastro},
-    {path: 'telaInicial', component: TelaInicial}
-
+    {path: 'telaInicial', component: TelaInicial},
+    {path: 'telaInicialEncaregados', component: TelaInicialEncarregados}
 ];
 
 
