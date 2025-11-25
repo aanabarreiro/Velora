6fad0b41d68f5dcb22d9aba9e74e8e66f14afa47
Ana Beatriz Arteiro Barreiro <anabeatrizarteiro@gmail.com>
2025-11-25 13:44:36 -0300

    Estruturação dos novos componentes

--- production/src/app/componentes/detalhe-parada/detalhe-parada.ts
+++ production/src/app/componentes/detalhe-parada/detalhe-parada.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-detalhe-parada',
@@ -8,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class DetalheParada {
 
+  orderId = 'OS-12345';
+  stopType = '';
+  stopTypes = ['Manutenção', 'Falta de material', 'Qualidade', 'Outro'];
+  detail = '';
+
+  cancel() {
+    this.detail = '';
+    this.stopType = '';
+    alert('Parada cancelada');
+  }
+
+  submit() {
+    console.log({ orderId: this.orderId, stopType: this.stopType, detail: this.detail });
+    alert('Enviado');
+  }
 }
